08f62d93c61ab5ad440703356f8fbf0e209675b6
Thoms971 <tkd-thomas-971@hotmail.fr>
2018-12-18 22:37:53 +0200

    Updates

---
+++ precache-manifest.0d44480f912206b0d58df726aa18a107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6de3c2d303f75dc4b17b",
+    "url": "/spms-gay_thomas/static/js/runtime~main.6de3c2d3.js"
+  },
+  {
+    "revision": "5c6975c0f2934d5233bd",
+    "url": "/spms-gay_thomas/static/js/main.5c6975c0.chunk.js"
+  },
+  {
+    "revision": "1d714d2257181748a59b",
+    "url": "/spms-gay_thomas/static/js/1.1d714d22.chunk.js"
+  },
+  {
+    "revision": "5c6975c0f2934d5233bd",
+    "url": "/spms-gay_thomas/static/css/main.d7bf9f29.chunk.css"
+  },
+  {
+    "revision": "34086f7a8234875a83a788f2807cff6e",
+    "url": "/spms-gay_thomas/index.html"
+  }
+];
